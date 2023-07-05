0c3e14425cf760ba62082fa2ad42b6aef478f806
Lucas Feksa Hickmann <lucasfhickmann@gmail.com>
2023-07-05 15:24:48 -0300

    Separate JsonTokenizer from JsonToken and JsonTokenType

---
+++ src/LiteDB.Tests/Utils/Crc8_Tests.cs
@@ -0,0 +1,52 @@
+﻿namespace LiteDB.Tests.Internals.Engine;
+
+public class Crc8_Tests
+{
+    [Fact]
+    public void Crc8_Crc8_Theory()
+    {
+        // Arrange
+        var sut = new CheckpointActions();
+
+        // datapages ends here
+        var lastPageID = 10;
+
+        // adding log pages at position
+        var logPages = new List<PageHeader>
+        {
+            new PageHeader { PositionID = 17, PageID = 24, TransactionID = 1, IsConfirmed = true }
+        };
+
+        // update lastPageID
+        lastPageID = Math.Max(lastPageID, logPages.Max(x => x.PageID));
+
+        // get start temp positionID and confirm transactions
+        var startTempPositionID = Math.Max(lastPageID, logPages[^1].PositionID) + 1;
+        var confirmedTransactions = new HashSet<int>(logPages.Where(x => x.IsConfirmed).Select(x => x.TransactionID));
+
+        // define temp pages
+        var tempPages = new List<PageHeader>
+        {
+            //new PageHeader { PositionID = 23, PageID = 24, TransactionID = 2, IsConfirmed = true } // 25
+        };
+
+        // Act
+        var actions = sut.GetActions(
+            logPages,
+            confirmedTransactions,
+            lastPageID,
+            startTempPositionID,
+            tempPages).ToArray();
+
+
+        // Asserts
+        actions.Length.Should().Be(5);
+
+        // action #0
+        actions[0].Action.Should().Be(CheckpointActionEnum.CopyToTempFile);
+        actions[0].PositionID.Should().Be(19);
+        actions[0].TargetPositionID.Should().Be(24);
+        actions[0].MustClear.Should().BeFalse();
+
+    }
+}
---
+++ src/LiteDB/Utils/JsonToken.cs
@@ -0,0 +1,78 @@
+﻿namespace LiteDB;
+/// <summary>
+/// Represent a single string token
+/// </summary>
+internal class JsonToken
+{
+    public JsonToken(JsonTokenType tokenType, string value, long position)
+    {
+        this.Position = position;
+        this.Value = value;
+        this.Type = tokenType;
+    }
+
+    public JsonTokenType Type { get; private set; }
+    public string Value { get; private set; }
+    public long Position { get; private set; }
+
+    /// <summary>
+    /// Expect if token is type (if not, throw UnexpectedToken)
+    /// </summary>
+    public JsonToken Expect(JsonTokenType type)
+    {
+        if (this.Type != type)
+        {
+            throw new Exception();
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// Expect for type1 OR type2 (if not, throw UnexpectedToken)
+    /// </summary>
+    public JsonToken Expect(JsonTokenType type1, JsonTokenType type2)
+    {
+        if (this.Type != type1 && this.Type != type2)
+        {
+            throw new Exception();
+        }
+
+        return this;
+    }
+
+    /// <summary>
+    /// Expect for type1 OR type2 OR type3 (if not, throw UnexpectedToken)
+    /// </summary>
+    public JsonToken Expect(JsonTokenType type1, JsonTokenType type2, JsonTokenType type3)
+    {
+        if (this.Type != type1 && this.Type != type2 && this.Type != type3)
+        {
+            throw new Exception();
+        }
+
+        return this;
+    }
+
+    public JsonToken Expect(string value, bool ignoreCase = true)
+    {
+        if (!this.Is(value, ignoreCase))
+        {
+            throw new Exception();
+        }
+
+        return this;
+    }
+
+    public bool Is(string value, bool ignoreCase = true)
+    {
+        return
+            this.Type == JsonTokenType.Word &&
+            value.Equals(this.Value, ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+    }
+
+    public override string ToString()
+    {
+        return this.Value + " (" + this.Type + ")";
+    }
+}
