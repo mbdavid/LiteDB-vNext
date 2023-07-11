8702c82e19ab7e7feeff9ce5cd6dcc8378d664a9
Lucas Feksa Hickmann <lucasfhickmann@gmail.com>
2023-07-11 13:53:30 -0300

    Adds IndexScanEnumerator class

---
+++ src/LiteDB.Tests/Internals/Engine/Query/Indexes/IndexEqualsEnumerator_Tests.cs
@@ -0,0 +1,36 @@
+﻿namespace LiteDB.Tests.Internals.Engine;
+
+public class IndexEqualsEnumerator_Tests
+{
+    [Fact]
+    public void MoveNextAsync()
+    {
+        #region Arrange
+        var indexDocument = new IndexDocument()
+        {
+            Slot = 1,
+            Name = "IndexName",
+            Expr = null,
+            Unique = true,
+            Head = new PageAddress(0, 1),
+            Tail = new PageAddress(10, 1)
+
+        };
+        var _sut = new IndexEqualsEnumerator(1, indexDocument, Collation.Default);
+
+        PipeContext pipeContext = new PipeContext();
+        var value = new ValueTask<PipeValue>();
+
+        #endregion
+
+
+        #region Act
+        value = _sut.MoveNextAsync(pipeContext);
+        #endregion
+
+
+        #region Asserts
+        value.Should().Be(1);
+        #endregion
+    }
+}
---
+++ src/LiteDB/Engine/Query/Indexes/IndexScanEnumerator.cs
@@ -0,0 +1,109 @@
+﻿namespace LiteDB.Engine;
+
+internal class IndexScanEnumerator : IPipeEnumerator
+{
+    private readonly Collation _collation;
+
+    private readonly IndexDocument _indexDocument;
+    private readonly BsonValue _value;
+
+    private bool _init = false;
+    private bool _eof = false;
+
+    private PageAddress _prev = PageAddress.Empty; // all nodes from left of first node found
+    private PageAddress _next = PageAddress.Empty; // all nodes from right of first node found
+
+    public IndexScanEnumerator(
+        BsonValue value, 
+        IndexDocument indexDocument, 
+        Collation collation)
+    {
+        _value = value;
+        _indexDocument = indexDocument;
+        _collation = collation;
+    }
+
+    public async ValueTask<PipeValue> MoveNextAsync(PipeContext context)
+    {
+        if (_eof) return PipeValue.Empty;
+
+        var indexService = context.IndexService;
+        var dataBlock = PageAddress.Empty;
+
+        // in first run, look for index node
+        if (!_init)
+        {
+            var nodeRef = await indexService.FindAsync(_indexDocument, _value, false, Query.Ascending);
+
+            // if node was not found, end enumerator
+            if (nodeRef is null)
+            {
+                _init = _eof = true;
+                return PipeValue.Empty;
+            }
+
+            var node = nodeRef.Value.Node;
+
+            // current node to return
+            dataBlock = node.DataBlock;
+
+            if (_indexDocument.Unique)
+            {
+                _eof = true;
+            }
+            else
+            {
+                // get pointer to next/prev at level 0
+                _prev = node.Prev[0];
+                _next = node.Next[0];
+            }
+
+            _init = true;
+        }
+        // first go forward
+        else if (!_next.IsEmpty)
+        {
+            var nodeRef = await indexService.GetNodeAsync(_next, false);
+            var node = nodeRef.Node;
+
+            var isEqual = _collation.Equals(_value, node.Key);
+
+            if (isEqual)
+            {
+                dataBlock = node.DataBlock;
+
+                _next = nodeRef.Node.Next[0];
+            }
+            else
+            {
+                _eof = true;
+            }
+        }
+        // and than, go backward
+        else if (!_prev.IsEmpty)
+        {
+            var nodeRef = await indexService.GetNodeAsync(_prev, false);
+            var node = nodeRef.Node;
+
+            var isEqual = _collation.Equals(_value, node.Key);
+
+            if (isEqual)
+            {
+                dataBlock = node.DataBlock;
+
+                _next = nodeRef.Node.Prev[0];
+            }
+            else
+            {
+                _eof = true;
+            }
+        }
+
+        // return current dataBlock rowID
+        return new PipeValue(dataBlock);
+    }
+
+    public void Dispose()
+    {
+    }
+}
