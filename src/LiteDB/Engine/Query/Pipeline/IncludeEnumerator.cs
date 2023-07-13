e037510085edf0a42a204fc274431935835b13fe
Mauricio David <mauricio.david@gmail.com>
2023-07-13 14:38:38 -0300

    Include enumetrator

---
+++ src/LiteDB/Engine/Query/Pipeline/IncludeEnumerator.cs
@@ -0,0 +1,48 @@
+﻿namespace LiteDB.Engine;
+
+internal class IncludeEnumerator : IPipeEnumerator
+{
+    private readonly BsonExpression _pathExpr;
+    private readonly Collation _collation;
+    private readonly IPipeEnumerator _enumerator;
+
+    private bool _init = false;
+    private bool _eof = false;
+
+    public IncludeEnumerator(BsonExpression pathExpr, Collation collation, IPipeEnumerator enumerator)
+    {
+        _pathExpr = pathExpr;
+        _collation = collation;
+        _enumerator = enumerator;
+    }
+
+    public async ValueTask<PipeValue> MoveNextAsync(PipeContext context)
+    {
+        if (_eof) return PipeValue.Empty;
+
+        while (!_eof)
+        {
+            var item = await _enumerator.MoveNextAsync(context);
+
+            if (item.IsEmpty)
+            {
+                _init = _eof = true;
+            }
+            else
+            {
+                // initialize current key with first key
+                if (!_init)
+                {
+                    _init = true;
+
+                }
+
+            }
+        }
+
+    }
+
+    public void Dispose()
+    {
+    }
+}
