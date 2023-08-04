fb13ecf8a826b818cde21376a95021e041c862b2
Lucas Feksa Hickmann <lucasfhickmann@gmail.com>
2023-08-04 13:46:16 -0300

    Expression_Tests

---
+++ src/LiteDB.Tests/Expressions/BsonExpressionParser_Tests.cs
@@ -0,0 +1,24 @@
+﻿using NSubstitute.ExceptionExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB.Tests.Expressions;
+
+public class BsonExpressionParser_Tests
+{
+
+    [Theory]
+    [InlineData("1 BETWEEN 1")]
+    [InlineData("{a:1 b:1}")]
+    [InlineData("[1,2 3]")]
+    [InlineData("true OR (x>1")]
+    [InlineData("UPPER('abc'")]
+    [InlineData("INDEXOF('abc''b')")]
+    public void Execute_Constants(string expr)
+    {
+        Assert.Throws<LiteException> (() => BsonExpression.Create(expr));
+    }
+}
